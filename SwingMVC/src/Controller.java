6ecfe3292ac645ebc8abd5874440a1a52843ff5d
PatH5 <43816130+harshpatel5@users.noreply.github.com>
2025-10-05 18:56:19 -0400

    working code for scanner

--- SwingMVC/src/Controller.java
+++ SwingMVC/src/Controller.java
@@ -1,43 +1,31 @@
-import javax.swing.JOptionPane;
- 
 public class Controller {
-	 private Model model;
-	 private View view;
-	 
-	 public Controller(Model m, View v) {
-	  model = m;
-	  view = v;
-	  initView();
-	 }
-	 
-	 public void initView() {
-	  view.getFirstnameTextfield().setText(model.getFirstname());
-	  view.getLastnameTextfield().setText(model.getLastname());
-	 }
-	 
-	 public void initController() {
-	  view.getFirstnameSaveButton().addActionListener(e -> saveFirstname());
-	  view.getLastnameSaveButton().addActionListener(e -> saveLastname());
-	  view.getHello().addActionListener(e -> sayHello());
-	  view.getBye().addActionListener(e -> sayBye());
-	 }
-	 
-	 private void saveFirstname() {
-	  model.setFirstname(view.getFirstnameTextfield().getText());
-	  JOptionPane.showMessageDialog(null, "Firstname saved : " + model.getFirstname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void saveLastname() {
-	  model.setLastname(view.getLastnameTextfield().getText());
-	  JOptionPane.showMessageDialog(null, "Lastname saved : " + model.getLastname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void sayHello() {
-	  JOptionPane.showMessageDialog(null, "Hello " + model.getFirstname() + " " + model.getLastname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void sayBye() {
-	  System.exit(0);
-	 }
-	 
+	private Model model;
+	private View view;
+	private Scanner scanner;
+
+
+	public Controller(Model model, View view, Scanner scanner) {
+		this.model = model;
+		this.view = view;
+		this.scanner = scanner;
+
+		view.getScanButton().addActionListener(e -> {
+			String upc = scanner.generateUPC();
+			if (upc != null) {
+				model.addItemByUPC(upc);
+				updateView();
+			}
+		});
+	}
+
+
+
+	private void updateView() {
+		StringBuilder sb = new StringBuilder();
+		for (Model.Product p : model.getScannedItems()) {
+			sb.append(p.name).append(" - $").append(p.price).append("\n");
+		}
+		view.getItemsArea().setText(sb.toString());
+		view.getSubtotalLabel().setText(String.format("Subtotal: $%.2f", model.getSubtotal()));
+	}
 }
